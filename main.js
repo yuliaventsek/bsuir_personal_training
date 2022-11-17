ed117df043ec5d1e9d0944c7aba6b9894503bb4a
Yulia Ventsek <yulia.ventsek@gmail.com>
2022-11-18 00:03:05 +0300

    50%

---
+++ main.js
@@ -0,0 +1,106 @@
+const tabs = document.getElementById('tabs');
+const content = document.querySelectorAll('.content');
+
+const changeClass = el => {
+    for (let i = 0; i < tabs.children.length; i++) {
+        tabs.children[i].classList.remove('active');
+    }
+    el.classList.add('active');
+}
+
+tabs.addEventListener('click', e => {
+    const currTab = e.target.dataset.btn;
+    changeClass(e.target);
+    for (let i = 0; i < content.length; i++) {
+        content[i].classList.remove('active');
+        if (content[i].dataset.content === currTab) {
+            content[i].classList.add('active');
+        }
+    }
+})
+
+
+
+
+
+
+
+
+
+
+const prev = document.getElementById('btn-prev'),
+    next = document.getElementById('btn-next'),
+    slides = document.querySelectorAll('.slide');
+
+
+let index = 0;
+
+const activeSlide = n => {
+    for (slide of slides) {
+         slide.classList.remove('active');
+    }
+    slides[n].classList.add('active');
+}
+
+
+const prepareCurrentSlide = ind => {
+    activeSlide(index);
+}
+
+const nextSlide = () => {
+    if (index == slides.length - 1) {
+        index = 0;
+        prepareCurrentSlide(index);
+    } else {
+        index++;
+        prepareCurrentSlide(index);
+    }
+}
+
+const prevSlide = () => {
+    if (index == 0) {
+        index = slides.length - 1;
+        prepareCurrentSlide(index);
+    } else {
+        index--;
+        prepareCurrentSlide(index);
+    }
+}
+
+next.addEventListener('click', nextSlide);
+prev.addEventListener('click', prevSlide);
+
+
+
+
+
+
+
+let select = function () {
+    let selectHeader = document.querySelectorAll('.select__header');
+    let selectItem = document.querySelectorAll('.select__item');
+
+    selectHeader.forEach(item => {
+        item.addEventListener('click', selectToggle)
+    });
+
+    selectItem.forEach(item => {
+        item.addEventListener('click', selectChoose)
+    });
+
+    function selectToggle() {
+        this.parentElement.classList.toggle('is-active');
+    }
+
+    function selectChoose() {
+        let text = this.innerText,
+            select = this.closest('.select'),
+            currentText = select.querySelector('.select__current');
+        currentText.innerText = text;
+        select.classList.remove('is-active');
+
+    }
+
+};
+
+select();
