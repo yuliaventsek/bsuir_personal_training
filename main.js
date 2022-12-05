b1717f4801af8d939464d2a6185da6595ece3476
Yulia Ventsek <yulia.ventsek@gmail.com>
2022-12-05 21:21:13 +0300

    without adaptive

--- main.js
+++ main.js
@@ -1,3 +1,40 @@
+const btnUp = {
+    el: document.querySelector('.btn-up'),
+    show() {
+      // удалим у кнопки класс btn-up_hide
+      this.el.classList.remove('btn-up_hide');
+    },
+    hide() {
+      // добавим к кнопке класс btn-up_hide
+      this.el.classList.add('btn-up_hide');
+    },
+    addEventListener() {
+      // при прокрутке содержимого страницы
+      window.addEventListener('scroll', () => {
+        // определяем величину прокрутки
+        const scrollY = window.scrollY || document.documentElement.scrollTop;
+        // если страница прокручена больше чем на 400px, то делаем кнопку видимой, иначе скрываем
+        scrollY > 400 ? this.show() : this.hide();
+      });
+      // при нажатии на кнопку .btn-up
+      document.querySelector('.btn-up').onclick = () => {
+        // переместим в начало страницы
+        window.scrollTo({
+          top: 0,
+          left: 0,
+          behavior: 'smooth'
+        });
+      }
+    }
+  }
+  
+  btnUp.addEventListener();
+
+
+
+
+
+
 const tabs = document.getElementById('tabs');
 const content = document.querySelectorAll('.content');
 
@@ -67,8 +104,46 @@ const prevSlide = () => {
     }
 }
 
+// const nextSlide2 = () => {
+//     if (index == slides.length - 1) {
+//         index = 0;
+//         prepareCurrentSlide(index);
+//     } else {
+//         index++;
+//         prepareCurrentSlide(index);
+//     }
+// }
+
+// slides.addEventListener('click', nextSlide2);
 next.addEventListener('click', nextSlide);
 prev.addEventListener('click', prevSlide);
+
+// mouseenter and mouseleave
+this._el.addEventListener('mouseenter', () => {
+    this._autoplay('stop');
+  });
+  this._el.addEventListener('mouseleave', () => {
+    this._autoplay();
+  });
+  // swipe
+  if (this._config.swipe) {
+    const options = this.constructor.checkSupportPassiveEvents() ? { passive: false } : false;
+    this._el.addEventListener('touchstart', onSwipeStart, options);
+    this._el.addEventListener('touchmove', onSwipeMove, options);
+    this._el.addEventListener('mousedown', onSwipeStart);
+    this._el.addEventListener('mousemove', onSwipeMove);
+    document.addEventListener('touchend', onSwipeEnd);
+    document.addEventListener('mouseup', onSwipeEnd);
+    document.addEventListener('mouseout', onSwipeEnd);
+  }
+  this._el.addEventListener('dragstart', (e) => {
+    e.preventDefault();
+  });
+
+
+
+
+
 
 
 
